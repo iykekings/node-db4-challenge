709fd42d65a184845ba07c8ae5b696ce89f273ab
Ikechukwu <iykekings36@gmail.com>
2019-07-18 15:37:49 +0100

    run migrations and arranged folder structure

--- knexfile.js
+++ knexfile.js
@@ -5,19 +5,19 @@ module.exports = {
     client: 'sqlite3',
     connection: {
       filename: './data/recipes.db3'
-    }
-  },
-  useNullAsDefault: true,
-  migrations: {
-    directory: './data/migrations'
-  },
-  seeds: {
-    directory: './data/seeds'
-  },
-  pool: {
-    afterCreate: (conn, done) => {
-      // runs after a connection is made to the sqlite engine
-      conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        // runs after a connection is made to the sqlite engine
+        conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
+      }
     }
   }
 };
